065976dd02f660888d2fdac9e2bd2aa5989d18da
Dan Buhrman <dan.buhrman@sencha.com>
2017-02-25 15:12:54 -0500

    giphify the bot.

--- globals.js
+++ globals.js
@@ -1,15 +1,4 @@
 'use strict';
 
-const fs = require('fs'),
-    path = require('path'),
-    secretsFile = path.join(__dirname, 'secrets.json');
-
-// Use secrets.json locally, but deployed app use env vars.
-try {
-    const secrets = JSON.parse(fs.readFileSync(secretsFile, 'utf-8'));
-    Object.assign(global, secrets);
-} catch(err) {
-    global.BOT_ID = process.env.BOT_ID;
-}
-
+global.BOT_ID = process.env.BOT_ID;
 global.PORT = process.env.PORT || 5500;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('./globals');
 const express = require('express'),
     https = require('https'),
     bodyParser = require('body-parser'),
+    giphyApi = require('giphy-api')(),
     app = express();
 
 // JSON parser middleware
@@ -12,37 +13,59 @@ app.use(bodyParser.json());
 
 // Ping
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    if(req.query && req.query.q) {
+        giphyApi.search(req.query.q).then(giphys => {
+            if(giphys.data && giphys.data.length > 0) {
+                const randomGif = giphys.data[Math.floor(Math.random() * giphys.data.length)];
+                sendBotMessage(`http://i.giphy.com/${randomGif.id}.gif`);
+                res.sendStatus(200);
+            } else {
+                res.send('No gifs returned.');
+            }
+        }).catch(err => {
+            res.status(400).send(`Error with giphy API: ${err}`);
+        });
+    } else {
+        res.send("I'm alive!!! WHHAHHAHAH");
+    }
 });
 
 // Groupme messages handler
 app.post('/', (req, res) => {
     console.log('Received groupme message', req.body);
 
-    if(req.body.sender_type !== 'bot') {
-        const botRequest = https.request({
-            hostname: 'api.groupme.com',
-            path: '/v3/bots/post',
-            method: 'POST'
-        }, res => {
-            console.log('Status code response from groupme post: ', res && res.statusCode);
-        });
+    // Don't care about this message, move-on with our lives.
+    const textMatch = req.body.match(/^giphy (.*)$/);
+    if(req.body.sender_type === 'bot' || !textMatch) return res.sendStatus(200);
 
-        const body = {
-            bot_id: BOT_ID,
-            text: req.body.text
-        };
+    giphyApi.search(textMatch[1]).then(giphys => {
+        if(giphys.data && giphys.data.length > 0) {
+            const randomGif = giphys.data[Math.floor(Math.random() * giphys.data.length)];
+            sendBotMessage(`http://i.giphy.com/${randomGif.id}.gif`);
+        } else {
+            sendBotMessage(`No giphys found for search ${textMatch[1]}. :'(`);
+        }
 
-        botRequest.on('error', err => {
-            console.error('Error from groupme post: ', err);
-        }).on('timeout', err => {
-            console.error('Timeout from groupme post:', err);
-        }).end(JSON.stringify(body));
-    }
-
-    res.sendStatus(200);
+        res.sendStatus(200);
+    });
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+const sendBotMessage = text => {
+    const body = { bot_id: BOT_ID, text};
+    console.log('Sending bot post request: ', body);
+    https.request({
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+    }, res => {
+        console.log('Status code response from groupme post: ', res && res.statusCode);
+    }).on('error', err => {
+        console.error('Error from groupme post: ', err);
+    }).on('timeout', err => {
+        console.error('Timeout from groupme post:', err);
+    }).end(JSON.stringify(body));
+};
