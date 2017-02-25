68b21214872feefaf30b3a5aed3d72a936e9c233
Dan Buhrman <dan.buhrman@sencha.com>
2017-02-25 13:06:34 -0500

    Fix extra paran.

--- globals.js
+++ globals.js
@@ -4,6 +4,6 @@ const fs = require('fs'),
     path = require('path'),
     secretsFile = path.join(__dirname, 'secrets.json');
 
-globals.secrets = JSON.parse(fs.readFileSync(secretsFile, 'utf-8')));
+globals.secrets = JSON.parse(fs.readFileSync(secretsFile, 'utf-8'));
 
 globals.PORT = process.env.PORT || 5500;
