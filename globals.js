e9c330377cf7e0f6084061444f451fc3743dd616
Dan Buhrman <dan.buhrman@sencha.com>
2017-02-26 15:19:47 -0500

    Fix stupid init error with bot_ids.

--- globals.js
+++ globals.js
@@ -5,7 +5,7 @@
 // based on the source group_id.
 global.BOTS = process.env.BOT_IDS.split(';').reduce((res, bot) => {
     const botParts = bot.split('=');
-    res[botParts[0]] = res[botParts[1]];
+    res[botParts[0]] = botParts[1];
     return res;
 }, {});
 global.PORT = process.env.PORT || 5500;
